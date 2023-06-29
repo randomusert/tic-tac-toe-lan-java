53ef918a2cd1b7b7c775c0926b9014a192cf9260
randomusert <127417963+randomusert@users.noreply.github.com>
2023-06-29 15:39:13 +0300

    third commit

--- src/com/randomusert/tic_tac_toe_lan/tictactoe.java
+++ src/com/randomusert/tic_tac_toe_lan/tictactoe.java
@@ -1,5 +1,29 @@
 package com.randomusert.tic_tac_toe_lan;
 
-public class tictactoe {
-    
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import java.lang.Thread;
+
+public class tictactoe implements Runnable {
+    //vars
+    private String ip = "1300.1.0.3";
+    private int port = 30000;
+    private Scanner scanner = new Scanner(System.in);
+    private JFrame frame;
+    private final int WIDTH = 500;
+    private final int HEIGHT = 590;
+    private Thread thread;
+
+    private Painter painter;
+    public tictactoe() {
+
+    }
+    public void run() {
+
+    }
+    @SuppressWarnings("unused")
+     public static void main(String[] args) {
+        tictactoe tictactoe = new tictactoe();
+    }
 }
